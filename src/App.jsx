554a93c0529e9de8b09ba3e18b9cf45be655eeb0
arvind <Arvindpal6706@gmail.com>
2025-05-06 00:25:49 +0530

    third-commint

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 
 function App() {
   return (
-    <div className="flex flex-col items-center h-screen">
-      <h1 className="font-bold text-black text-center mt-8">Tic Tok Game</h1>
-      <div className="w-[500px] mt-4">
+    <div className="flex flex-col items-center h-screen px-4">
+      <h1 className="font-bold text-black text-center mt-8 text-xl md:text-3xl">Tic Tok Game</h1>
+      <div className="w-full max-w-md mt-4">
         <Board />
       </div>
     </div>
