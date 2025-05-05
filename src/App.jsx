59903e44b72e4e8bb782c444c3f6596556f08d70
arvind <Arvindpal6706@gmail.com>
2025-05-06 00:06:47 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="font-bold  text-black text-center mb-4">Tic Tac Toe Game</h1>
-      <div className="w-[500px]">
+    <div className="flex flex-col items-center h-screen">
+      <h1 className="font-bold text-black text-center mt-8">Tic Tok Game</h1>
+      <div className="w-[500px] mt-4">
         <Board />
       </div>
     </div>
